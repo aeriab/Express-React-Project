45c59d311906ecd34158c042b5e363ae0a94403c
BrendanA1622 <89553639+BrendanA1622@users.noreply.github.com>
2025-02-15 21:20:05 -0800

    idk how to work with the .json :(

--- server/server.js
+++ server/server.js
@@ -2,7 +2,8 @@ const express = require('express')
 const app = express()
 
 app.get("/api", (req, res) => {
-    res.json({ "users": ["userOne", "userTwo", "userThree"], "passwords": ["passwordOne", "passwordTwo", "passwordThree"] })
+    res.json({ "users": ["userOne", "userTwo", "userThree"] })
+    res.json.push({ "passwords": ["passwordOne", "passwordTwo", "passwordThree"] })
 })
 
 app.listen(5000, () => { console.log("Server is running on port 5000") })
